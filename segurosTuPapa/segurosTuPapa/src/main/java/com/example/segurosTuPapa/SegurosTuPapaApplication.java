35a7b4f509cb264c3979c73a36e61ec91bfef731
semaxyD <88868446+semaxyD@users.noreply.github.com>
2024-04-01 16:51:32 -0500

    Add files via upload

---
+++ segurosTuPapa/segurosTuPapa/src/main/java/com/example/segurosTuPapa/SegurosTuPapaApplication.java
@@ -0,0 +1,13 @@
+package com.example.segurosTuPapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurosTuPapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurosTuPapaApplication.class, args);
+	}
+
+}
