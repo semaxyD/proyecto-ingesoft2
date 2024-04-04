2cfc556e73240c09c8e15f19bccd5534fae594f2
semaxyD <88868446+semaxyD@users.noreply.github.com>
2024-04-03 22:29:19 -0500

    Add files via upload

---
+++ autentication/autentication/src/main/java/api/autentication/AutenticationApplication.java
@@ -0,0 +1,13 @@
+package api.autentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticationApplication.class, args);
+	}
+
+}
